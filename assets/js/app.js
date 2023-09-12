10ef40ac88c4d8daf4bb05378b79c6b648828821
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2023-09-12 16:13:55 +0530

    remove extra  space

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
 let a = document.getElementById("nav")
+let flow = document.querySelector(".comp-img")
 function responsive() {
     a.classList.toggle("fix-lft")
+    flow.classList.toggle("flow-hidden")
 }
